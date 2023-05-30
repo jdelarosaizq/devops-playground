52f459b301d68bcc58d70356d7704cc6a40d4e3b
jorgeizquierdo <jorge.izquierdo@gbgplc.com>
2023-05-30 09:42:35 +0200

    push branches

--- run-push-branches.sh
+++ run-push-branches.sh
@@ -3,6 +3,11 @@ set -x
 OWNER=$1
 REPO=$2
 
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$OWNER/$REPO/actions/artifacts
+
 wget -L \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
